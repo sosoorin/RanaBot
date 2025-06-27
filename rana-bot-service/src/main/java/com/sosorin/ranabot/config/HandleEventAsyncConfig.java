6ee46007b277750047044184bd458036b93e6e50
Sosoorin <920009579@qq.com>
2025-06-27 13:02:45 +0800

    feat(plugin): 异步处理事件并优化线程池

---
+++ rana-bot-service/src/main/java/com/sosorin/ranabot/config/HandleEventAsyncConfig.java
@@ -0,0 +1,36 @@
+package com.sosorin.ranabot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author rana-bot
+ * @since 2025/6/27  12:45
+ */
+@Configuration
+public class HandleEventAsyncConfig{
+
+    private static final AtomicInteger POOL_NUMBER = new AtomicInteger(1);
+
+    @Bean("handleEventAsync")
+    public Executor getAsyncExecutor() {
+        ThreadFactory threadFactory = new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                return new Thread(null, r, "handleEventAsync-" + POOL_NUMBER.getAndIncrement(), 0);
+            }
+        };
+        return new ThreadPoolExecutor(
+                2,
+                4,
+                5,
+                TimeUnit.SECONDS,
+                new LinkedBlockingDeque<>(100),
+                threadFactory,
+                new ThreadPoolExecutor.DiscardOldestPolicy()
+        );
+    }
+}
