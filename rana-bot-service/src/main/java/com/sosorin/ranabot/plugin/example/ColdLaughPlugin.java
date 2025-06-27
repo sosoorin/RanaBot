00f299d68eaa0f3904a00b862e8657b43b0952c7
shenglin <920009579@qq.com>
2025-06-27 17:32:43 +0800

    feat(plugin): 添加谐音梗生成器插件

---
+++ rana-bot-service/src/main/java/com/sosorin/ranabot/plugin/example/ColdLaughPlugin.java
@@ -0,0 +1,145 @@
+package com.sosorin.ranabot.plugin.example;
+
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.extra.pinyin.PinyinUtil;
+import com.sosorin.ranabot.annotation.RanaPlugin;
+import com.sosorin.ranabot.entity.event.message.BaseMessageEvent;
+import com.sosorin.ranabot.entity.event.message.GroupMessageEvent;
+import com.sosorin.ranabot.entity.event.message.PrivateMessageEvent;
+import com.sosorin.ranabot.entity.message.Message;
+import com.sosorin.ranabot.model.EventBody;
+import com.sosorin.ranabot.plugin.AbstractPlugin;
+import com.sosorin.ranabot.service.IWebSocketService;
+import com.sosorin.ranabot.util.EventParseUtil;
+import com.sosorin.ranabot.util.MessageUtil;
+import com.sosorin.ranabot.util.SendEntityUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author rana-bot
+ * @since 2025/6/27  16:29
+ */
+@RanaPlugin("谐音梗生成器")
+@Slf4j
+public class ColdLaughPlugin extends AbstractPlugin {
+    @Autowired
+    private IWebSocketService webSocketService;
+
+    private final Map<String, Object> PARAMS = new ConcurrentHashMap<>();
+
+    public ColdLaughPlugin() {
+        super("一个简单的谐音梗插件", "1.0.0", "rana-bot");
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+        // 这里只是示例，实际开发中可以从文件中读取关键词，并实现动态替换
+        List<String> keywords = List.of("抹茶芭菲~", "星巴克", "有趣的女人", "米村拌饭");
+        PARAMS.put("keywords", keywords);
+        log.info("谐音梗插件已启用，将会回复所有收到的包含 [{}] 的消息", keywords);
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+        log.info("谐音梗插件已禁用");
+    }
+
+    /**
+     * 获取插件参数
+     *
+     * @return 插件参数
+     */
+    @Override
+    public Map<String, Object> getParams() {
+        return PARAMS;
+    }
+
+    /**
+     * 设置插件参数
+     *
+     * @param params 插件参数
+     * @return 是否设置成功
+     */
+    @Override
+    public boolean setParams(Map<String, Object> params) {
+        // 这里只是示例，实际开发中可以将关键词写入数据库中，并读取
+        PARAMS.putAll(params);
+        log.info("[{}]插件参数已设置: {}", name, PARAMS);
+        return true;
+    }
+
+    @Override
+    public String handleEvent(EventBody eventBody) {
+        // 尝试将事件转换为消息事件
+        Optional<BaseMessageEvent> messageEvent = EventParseUtil.asMessageEvent(eventBody);
+
+        // 如果是消息事件，则返回对应冷笑话
+        if (messageEvent.isPresent()) {
+            BaseMessageEvent event = messageEvent.get();
+            List<Message> messages = event.getMessage();
+
+            List<String> keywordsList = new ArrayList<>();
+            Object keywords = PARAMS.get("keywords");
+            if (ObjectUtil.isNotNull(keywords) && keywords instanceof List) {
+                keywordsList = (List<String>) keywords;
+            }
+            String recText = MessageUtil.extractTextContent(messages);
+            String recFirstLetter = PinyinUtil.getFirstLetter(recText, "");
+            Map<String, List<String>> keywordsMap = new HashMap<>();
+            keywordsList.stream().forEach(keyword -> {
+                String firstLetter = PinyinUtil.getFirstLetter(keyword, "");
+                // 去掉除了大小写英文字母外的其他字符
+                firstLetter = firstLetter.replaceAll("[^a-zA-Z]", "");
+                if (keywordsMap.containsKey(firstLetter)) {
+                    keywordsMap.get(firstLetter).add(keyword);
+                } else {
+                    keywordsMap.put(firstLetter, new ArrayList<>(Collections.singletonList(keyword)));
+                }
+            });
+            List<Message> coldMsg = new ArrayList<>();
+            keywordsMap.forEach((k, v) -> {
+                if (recFirstLetter.contains(k)) {
+                    v.forEach(keyword -> {
+                        coldMsg.add(MessageUtil.createTextMessage(keyword));
+                    });
+                }
+            });
+            if (!coldMsg.isEmpty()) {
+                Message replyTextMessage = MessageUtil.createReplyMessage(event.getMessageId().toString());
+                // 从coldMsg的list中随机取一条消息
+                Message simpleMessage = coldMsg.get(new Random().nextInt(coldMsg.size()));
+                List<Message> replyMessages = List.of(replyTextMessage, simpleMessage);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                // 创建回复消息
+                String bodyStr = "";
+                if (event instanceof PrivateMessageEvent) {
+                    bodyStr = SendEntityUtil.buildSendPrivateMessageStr(event.getUserId().toString(), replyMessages);
+                } else if (event instanceof GroupMessageEvent) {
+                    bodyStr = SendEntityUtil.buildSendGroupMessageStr(((GroupMessageEvent) event).getGroupId().toString(), replyMessages);
+                }
+                log.info("发送消息: {}", bodyStr);
+                webSocketService.send(bodyStr);
+            }
+        }
+
+        // 如果不是消息事件，则返回null表示不处理
+        return null;
+    }
+
+
+    @Override
+    public boolean canHandle(EventBody eventBody) {
+        // 只处理私聊消息事件
+        return eventBody instanceof BaseMessageEvent;
+    }
+}
