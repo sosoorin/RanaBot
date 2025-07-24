a572bd45bc7d16444188c10172d65f34b418dab6
Sosoorin <920009579@qq.com>
2025-07-25 06:16:19 +0800

    feat(config): 优化插件目录配置并添加数据目录支持
    - 添加 pluginDataDir 字段，用于配置插件数据目录
    - 实现 getPluginDir 和 getPluginDataDir 方法，返回绝对路径
    - 使用 Hutool 工具库处理文件路径

--- rana-bot-plugin/src/main/java/com/sosorin/ranabot/config/PluginConfigProperties.java
+++ rana-bot-plugin/src/main/java/com/sosorin/ranabot/config/PluginConfigProperties.java
@@ -1,8 +1,11 @@
 package com.sosorin.ranabot.config;
 
+import cn.hutool.core.io.FileUtil;
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -13,12 +16,22 @@ import java.util.List;
 @Configuration
 @ConfigurationProperties(prefix = "bot")
 @Data
+@Slf4j
 public class PluginConfigProperties {
 
     private List<String> enabledPlugins;
 
     private String pluginDir = "plugins";
+    
+    private String pluginDataDir;
 
-    private String pluginDataDir = pluginDir + "/data";
+    public String getPluginDir() {
+        return FileUtil.getAbsolutePath(pluginDir);
+    }
+
+    public String getPluginDataDir() {
+        String dataDir = StringUtils.hasText(pluginDataDir) ? pluginDataDir : pluginDir + "/data";
+        return FileUtil.getAbsolutePath(dataDir);
+    }
 
 }
